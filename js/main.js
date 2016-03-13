9a2362e43744ec0efa63f058fb1a8f171064bbb0
mfasman95 <matthew.f4sm4n@gmail.com>
2016-03-13 15:17:38 -0400

    Add comments

--- js/main.js
+++ js/main.js
@@ -1,15 +1,20 @@
 var bulletHeck = bulletHeck || {};
 
 bulletHeck.main = {
+    //variables go here
+    
+    //preload function for calling via phaser
     preload : function() {
         game.load.image('sky', 'assets/sky.png');
     },
-
+    
+    //create function for calling via phaser
     create : function() {
         //  A simple background for our game
         game.add.sprite(0, 0, 'sky');
     },
-
+    
+    //update function for calling via phaser
     update : function() {
     },
 }
