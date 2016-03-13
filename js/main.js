7a66c57ce8d7f8f0acad81a06aec1b3c0bdee597
mfasman95 <matthew.f4sm4n@gmail.com>
2016-03-13 15:13:56 -0400

    bulletHeck changed to js module system. Game now moved out of global space.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-//Main file for Bullet Heck
+var bulletHeck = bulletHeck || {};
+
+bulletHeck.main = {
+    preload : function() {
+        game.load.image('sky', 'assets/sky.png');
+    },
+
+    create : function() {
+        //  A simple background for our game
+        game.add.sprite(0, 0, 'sky');
+    },
+
+    update : function() {
+    },
+}
