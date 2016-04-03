339438604a8e7f25b6c099862dfcc71c0369a8eb
mfasman95 <matthew.f4sm4n@gmail.com>
2016-04-03 14:16:43 -0400

    UI overhaul

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Project 2 for RMWAD, Spring 2016.
 Credit to:
 http://opengameart.org/content/technozomians-owl-spaceship
 http://opengameart.org/content/sci-fi-space-simple-bullets
+http://opengameart.org/content/several-heart-styles
--- js/UI.js
+++ js/UI.js
@@ -12,13 +12,14 @@ UI.main = {
     score: 0,
     text: null,
     
-    //buttons: GAME_STATE_functionButton
-    MAIN_MENU_playButton: undefined,
-    IN_LEVEL_pauseButton: undefined,
-    PAUSE_unpauseButton: undefined,
-    PAUSE_toMainMenuButton: undefined,
-    GAME_OVER_replayPrevLevelButton: undefined,
-    LEVEL_COMPLETE_nextLevelButton: undefined,
+    //Three hearts for player health
+    heart1: undefined,
+    heart2: undefined,
+    heart3: undefined,
+    
+    //array to contain buttons
+    buttonsToDrawArray: [], //an array of ints which draws buttons based on the int
+    buttonsToDestroyArray: [], //an array of all buttons that have been drawn
     
     gameState: undefined,
     
@@ -36,8 +37,14 @@ UI.main = {
     preload: function(){
         this.gameState = this.GAME_STATE.MAIN_MENU;
         
+        //background
         game.load.image('space', 'assets/background.jpg');
         
+        //hearts
+        game.load.image('fullHeart', 'assets/UI/Hearts/heart_sharp.png');
+        game.load.image('emptyHeart', 'assets/UI/Hearts/heart_sharp_empty.png');
+        
+        //UI buttons
         game.load.spritesheet('playButton', 'assets/UI/playButton.png', 96, 32);
         game.load.spritesheet('nextLevelButton', 'assets/UI/nextLevelButton.png', 96, 32);
         game.load.spritesheet('mainMenuButton', 'assets/UI/mainMenuButton.png', 96, 32);
@@ -49,6 +56,8 @@ UI.main = {
     },
     
     create: function(){
+        console.log("Gamestate: " + this.gameState);
+        
         if(this.gameState != this.GAME_STATE.PAUSE && this.gameState != this.GAME_STATE.IN_LEVEL){
             //  A simple background for our game
             game.add.sprite(0, 0, 'space');
@@ -56,44 +65,120 @@ UI.main = {
         
         //establish MAIN_MENU UI
         if(this.gameState == this.GAME_STATE.MAIN_MENU){
-            this.MAIN_MENU_playButton = game.add.button(game.world.centerX - 48, game.world.centerY, 'playButton', this.playGame, this, 2,1,0);
+            this.buttonsToDrawArray.push(0);
         }
         
         //establish IN_LEVEL UI
         if(this.gameState == this.GAME_STATE.IN_LEVEL){
+            
         }
         
         //establish PAUSE UI
         if(this.gameState == this.GAME_STATE.PAUSE){
-            this.PAUSE_unpauseButton = game.add.button(game.world.centerX - 48, game.world.centerY - 32, 'unpauseButton', this.unpause, this, 2,1,0);
-            this.PAUSE_toMainMenuButton = game.add.button(game.world.centerX - 48, game.world.centerY + 32, 'mainMenuButton', this.toMainMenu, this, 2,1,0);
+            this.buttonsToDrawArray.push(1);
+            this.buttonsToDrawArray.push(2);
         }
         
         //establish GAME_OVER UI
         if(this.gameState == this.GAME_STATE.GAME_OVER){
-            this.GAME_OVER_replayPrevLevelButton = game.add.button(game.world.centerX - 48, game.world.centerY, 'replayLevelButton', this.replayPrevLevel, this, 2,1,0);
+            this.buttonsToDrawArray.push(2);
+            this.buttonsToDrawArray.push(0);
         }
         
         //establish LEVEL_COMPLETE UI
         if(this.gameState == this.GAME_STATE.LEVEL_COMPLETE){
-            this.LEVEL_COMPLETE_nextLevelButton = game.add.button(game.world.centerX - 48, game.world.centerY, 'nextLevelButton', this.nextLevel, this, 2,1,0);
+            this.buttonsToDrawArray.push(3);
+            this.buttonsToDrawArray.push(2);
         }
+        
+        for (var i = 0; i < this.buttonsToDrawArray.length; i++){
+            switch (this.buttonsToDrawArray[i]){
+                case 0: //this.UI_BUTTON_TYPE.PLAY_BUTTON:
+                    var button = game.add.button(game.world.centerX - 48, game.world.centerY - (this.buttonsToDrawArray.length/2 * 40) + (i * 40), 'playButton', this.playGame, this, 2,1,0);
+                    this.buttonsToDestroyArray.push(button);
+                    break;
+                case 1: //this.UI_BUTTON_TYPE.UNPAUSE_BUTTON:
+                    var button = game.add.button(game.world.centerX - 48, game.world.centerY - (this.buttonsToDrawArray.length/2 * 40) + (i * 40), 'unpauseButton', this.unpause, this, 2,1,0);
+                    this.buttonsToDestroyArray.push(button);
+                    break;
+                case 2: //this.UI_BUTTON_TYPE.MAIN_MENU_BUTTON:
+                    var button = game.add.button(game.world.centerX - 48, game.world.centerY - (this.buttonsToDrawArray.length/2 * 40) + (i * 40), 'mainMenuButton', this.toMainMenu, this, 2,1,0);
+                    this.buttonsToDestroyArray.push(button);
+                    break;
+                case 3: //this.UI_BUTTON_TYPE.NEXT_LEVEL_BUTTON:
+                    var button = game.add.button(game.world.centerX - 48, game.world.centerY - (this.buttonsToDrawArray.length/2 * 40) + (i * 40), 'nextLevelButton', this.nextLevel, this, 2,1,0);
+                    this.buttonsToDestroyArray.push(button);
+                    break;
+                default:
+                    console.log("No Button");
+                    break;
+            }
+        } 
         
         //Add a listener for the pause button (P)
         this.keyP.onDown.add(this.pauseToggle, this, 0);
     },
     
     update: function(){
+        //print the game score to the top right
         var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
         if(this.text != null) this.text.destroy();
         this.text = game.add.text(game.width - 20, 10, "Score: " + this.score, style);
         this.text.x -= this.text.width;
+        //put hearts in the top left if you are in a game state where they are relevant
+        if (this.gameState == this.GAME_STATE.IN_LEVEL ||
+            this.gameState == this.GAME_STATE.LEVEL_COMPLETE ||
+            this.gameState == this.GAME_STATE.PAUSE ||
+            this.gameState == this.GAME_STATE.GAME_OVER){
+            switch (player.main.health){
+                case 3:
+                    this.heart1 = game.add.sprite(10, 10, 'fullHeart');
+                    this.heart1.scale.set(2.0,2.0);
+                    this.heart2 = game.add.sprite(50, 10, 'fullHeart');
+                    this.heart3 = game.add.sprite(90, 10, 'fullHeart');
+                    this.heart1.scale.set(2.5,2.5);
+                    this.heart2.scale.set(2.5,2.5);
+                    this.heart3.scale.set(2.5,2.5);
+                    break;
+                case 2:
+                    this.heart1 = game.add.sprite(10, 10, 'fullHeart');
+                    this.heart2 = game.add.sprite(50, 10, 'fullHeart');
+                    this.heart3 = game.add.sprite(90, 10, 'emptyHeart');
+                    this.heart1.scale.set(2.5,2.5);
+                    this.heart2.scale.set(2.5,2.5);
+                    this.heart3.scale.set(2.5,2.5);
+                    break;
+                case 1:
+                    this.heart1 = game.add.sprite(10, 10, 'fullHeart');
+                    this.heart2 = game.add.sprite(50, 10, 'emptyHeart');
+                    this.heart3 = game.add.sprite(90, 10, 'emptyHeart');
+                    this.heart1.scale.set(2.5,2.5);
+                    this.heart2.scale.set(2.5,2.5);
+                    this.heart3.scale.set(2.5,2.5);
+                    break;
+                default:
+                    this.heart1 = game.add.sprite(10, 10, 'emptyHeart');
+                    this.heart2 = game.add.sprite(50, 10, 'emptyHeart');
+                    this.heart3 = game.add.sprite(90, 10, 'emptyHeart');
+                    this.heart1.scale.set(2.5,2.5);
+                    this.heart2.scale.set(2.5,2.5);
+                    this.heart3.scale.set(2.5,2.5);
+                    break;
+            }
+        }
+                
     },
     
     //Begin playing the game from the main menu
     playGame: function() {
+        //update the gamestate
         this.gameState = this.GAME_STATE.IN_LEVEL;
-        this.MAIN_MENU_playButton.destroy();
+        //destroy all the buttons
+        for (var i = 0; i < this.buttonsToDestroyArray.length; i++){
+            this.buttonsToDestroyArray[i].destroy();
+        }
+        this.buttonsToDestroyArray = [];
+        this.buttonsToDrawArray = [];
         //rebuild the UI for the current gamestate
         this.create();
         //create the player objects
@@ -102,6 +187,59 @@ UI.main = {
         enemies.main.create();
     },
     
+    //Pause the game from in a level
+    pause: function(){
+        //update the gamestate
+        this.gameState = this.GAME_STATE.PAUSE;
+        //rebuild the UI for the current gamestate
+        this.create();
+    },
+    
+    //Unpause the game from the pause menu
+    unpause: function(){
+        //update the gamestate
+        this.gameState = this.GAME_STATE.IN_LEVEL;
+        //destroy all the buttons
+        for (var i = 0; i < this.buttonsToDestroyArray.length; i++){
+            this.buttonsToDestroyArray[i].destroy();
+        }
+        this.buttonsToDestroyArray = [];
+        this.buttonsToDrawArray = [];
+        //rebuild the UI for the current gamestate
+        this.create();
+    },
+    
+    //Return to the main menu from the pause menu
+    toMainMenu: function(){
+        //update the gamestate
+        this.gameState = this.GAME_STATE.MAIN_MENU;
+        //destroy all the buttons
+        for (var i = 0; i < this.buttonsToDestroyArray.length; i++){
+            this.buttonsToDestroyArray[i].destroy();
+        }
+        this.buttonsToDestroyArray = [];
+        this.buttonsToDrawArray = [];
+        //rebuild the UI for the current gamestate
+        this.create();
+    },
+    
+    //Move to the next level once you have completed a level
+    nextLevel: function(){
+        //update the gamestate
+        this.gameState = this.GAME_STATE.IN_LEVEL;
+        //destroy all the buttons
+        for (var i = 0; i < this.buttonsToDestroyArray.length; i++){
+            this.buttonsToDestroyArray[i].destroy();
+        }
+        this.buttonsToDestroyArray = [];
+        this.buttonsToDrawArray = [];
+        //rebuild the UI for the current gamestate
+        this.create(); 
+        player.main.create();
+        enemies.main.create();
+    },
+    
+        
     //Toggle the game state for purposes of pausing and unpausing
     pauseToggle: function() {
         //if the game is paused
@@ -117,51 +255,5 @@ UI.main = {
             return;
         }
         
-    },
-    
-    //Pause the game from in a level
-    pause: function(){
-        this.gameState = this.GAME_STATE.PAUSE;
-        //rebuild the UI for the current gamestate
-        this.create();
-    },
-    
-    //Unpause the game from the pause menu
-    unpause: function(){
-        this.gameState = this.GAME_STATE.IN_LEVEL;
-        this.PAUSE_unpauseButton.destroy();
-        this.PAUSE_toMainMenuButton.destroy();
-        //rebuild the UI for the current gamestate
-        this.create();
-    },
-    
-    //Return to the main menu from the pause menu
-    toMainMenu: function(){
-        this.gameState = this.GAME_STATE.MAIN_MENU;
-        this.PAUSE_unpauseButton.destroy();
-        this.PAUSE_toMainMenuButton.destroy();
-        //rebuild the UI for the current gamestate
-        this.create();
-    },
-    
-    //Restart the previous level when the replayPrevLevel button is pressed
-    replayPrevLevel: function(){
-        this.gameState = this.GAME_STATE.IN_LEVEL;
-        this.GAME_OVER_replayPrevLevelButton.destroy();
-        //rebuild the UI for the current gamestate
-        this.create();
-        //rebuild the player and the enemies
-        player.main.create();
-        enemies.main.create();
-    },
-    
-    //Move to the next level once you have completed a level
-    nextLevel: function(){
-        this.gameState = this.GAME_STATE.IN_LEVEL;
-        this.LEVEL_COMPLETE_nextLevelButton.destroy();
-        //rebuild the UI for the current gamestate
-        this.create(); 
-        player.main.create();
-        enemies.main.create();
     },
 }
